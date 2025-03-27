68c3a7a890b6295ce6c652b2bcfc5973f7c9e740
Peter Son <stayinvestor@gmail.com>
2025-03-27 15:06:06 -0400

    Section6 react todo app: vite + tailwindCSS

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <>
+            <h1 className="bg-purple-600 text-center uppercase md:text-right">
+                Todo
+            </h1>
+        </>
+    );
+};
+
+export default App;
